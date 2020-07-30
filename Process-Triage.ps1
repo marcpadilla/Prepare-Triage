a0e82304d7376775ba81e0ba4ed680906113f15c
Marc Padilla <marc@padil.la>
2020-07-29 20:41:06 -0700

    Add files via upload

--- Process-Triage.ps1
+++ Process-Triage.ps1
@@ -21,9 +21,9 @@ param(
 Write-Host "`nProcess-Triage by Marc Padilla (marc@padil.la)`n"
 
 # modify if necessary
-$TempDest = 'C:\Windows\Temp\angrydome\'
-$SevenZip = 'C:\Program Files\7-Zip\7z.exe'
-$Kape = 'C:\tools\kape\kape.exe'
+$TempDest = "C:\Windows\Temp\angrydome\"
+$SevenZip = "C:\Program Files\7-Zip\7z.exe"
+$Kape = "C:\tools\kape\kape.exe"
 $Location = Get-Location
 
 # check data source and required programs
@@ -35,8 +35,8 @@ foreach ($item in $SevenZip, $Kape, $Source) {
 }
 
 # concatenate a trailing \ if necessary
-if ($Destination[-1] -ne '\') {
-    $Destination += '\'
+if ($Destination[-1] -ne "\") {
+    $Destination += "\"
 }
 
 # create array with useful members
@@ -44,17 +44,18 @@ Set-Location -Path $Source
 $TriageDirectories = "DupTriage\", "KapeTriage\"
 $TriagePackages = Get-ChildItem -Path $TriageDirectories -Recurse | Where-Object -FilterScript {$_.FullName -match ".7z|.zip"} | Select FullName,BaseName,LastWriteTime
 foreach ($file in $TriagePackages) {
-    $UnderscoreCount = ($file.BaseName.Split('_DupTriage.7z')[0].Split('.zip')[0].ToCharArray() -eq "_").count # some windows hostnames contain "_" due to bootcamp and its obnoxious
+    $SensorId = $file.FullName.Split("\")[3].Split("_")[0]
+    $UnderscoreCount = ($file.BaseName.Split("_DupTriage.7z")[0].Split(".zip")[0].ToCharArray() -eq "_").count # some windows hostnames contain "_" due to bootcamp and its obnoxious
     if ($UnderscoreCount -gt 2) {
-        $file | Add-Member -MemberType NoteProperty -Name "HostName" -Value ($file.BaseName.Split('_DupTriage')[0].Split('.zip')[0].Split('_')[1..$UnderscoreCount] -join "_")
+        $file | Add-Member -MemberType NoteProperty -Name "HostName" -Value ($file.BaseName.Split("_DupTriage")[0].Split(".zip")[0].Split("_")[1..$UnderscoreCount] -join "_")
     }
     else {
-        $file | Add-Member -MemberType NoteProperty -Name "HostName" -Value $file.BaseName.Split('_DupTriage')[0].Split('.zip')[0].Split('_')[-1]
+        $file | Add-Member -MemberType NoteProperty -Name "HostName" -Value $file.BaseName.Split("_DupTriage")[0].Split(".zip")[0].Split("_")[-1]
     }
-    $file | Add-Member -MemberType NoteProperty -Name "TriageType" -Value $file.FullName.Split('\')[2]
-    $file | Add-Member -MemberType NoteProperty -Name "Incomplete" -Value ($file.FullName.Split('_')[-1] -eq 'INCOMPLETE.zip')
-    $file | Add-Member -MemberType NoteProperty -Name "Processed" -Value (Test-Path -Path ($Destination + $file.HostName + "_" + $file.LastWriteTime.ToString("yyyy-MM-ddTHHmmss")))
-    $file | Add-Member -MemberType NoteProperty -Name "SensorId" -Value $file.FullName.Split('\')[3].Split('_')[0]
+    $file | Add-Member -MemberType NoteProperty -Name "TriageType" -Value $file.FullName.Split("\")[2]
+    $file | Add-Member -MemberType NoteProperty -Name "Incomplete" -Value ($file.FullName.Split("_")[-1] -eq "INCOMPLETE.zip")
+    $file | Add-Member -MemberType NoteProperty -Name "Processed" -Value (Test-Path -Path ($Destination + $SensorId + "_" + $file.HostName + "_" + $file.LastWriteTime.ToString("yyyy-MM-ddTHHmmss")))
+    $file | Add-Member -MemberType NoteProperty -Name "SensorId" -Value $SensorId
 }
 $TriagePackages = $TriagePackages | Sort-Object LastWriteTime -Descending
 
@@ -93,7 +94,7 @@ else {
 }
 
 # check for lol slow cyberlab vm
-if ((Get-CimInstance -Class Win32_ComputerSystem | Select -ExpandProperty Domain) -eq 'cyber.local') {
+if ((Get-CimInstance -Class Win32_ComputerSystem | Select -ExpandProperty Domain) -eq "cyber.local") {
     $Cores = 2
 }
 else {
@@ -106,7 +107,7 @@ $TriagePackages | ForEach-Object -Parallel {
     $mdest = $using:Destination + $_.SensorId + "_" + $_.HostName + "_" + $_.LastWriteTime.ToString("yyyy-MM-ddTHHmmss")
     Write-Host "Processing" $_.FullName
     # decompress/unarchive, mount, etc.
-    if ($_.TriageType -eq 'DupTriage') {
+    if ($_.TriageType -eq "DupTriage") {
         $msource = $using:TempDest + $_.HostName
         # decompress, decompress... and decompress
         & $using:SevenZip x $_.FullName "-o$msource" 2>&1 | Out-Null
@@ -115,19 +116,19 @@ $TriagePackages | ForEach-Object -Parallel {
         & $using:SevenZip x "$msource\*.tar" "-o$msource" 2>&1 | Out-Null
         Remove-Item $msource\*.tar -Force
     }
-    if ($_.TriageType -eq 'KapeTriage') {
+    if ($_.TriageType -eq "KapeTriage") {
         Expand-Archive -Path $_.FullName -DestinationPath $using:TempDest -Force
-        $vhdx = $using:TempDest + $_.BaseName + '.vhdx'
+        $vhdx = $using:TempDest + $_.BaseName + ".vhdx"
         $msource = Mount-VHD -Path $vhdx -Passthru | Get-Disk | Get-Partition | Get-Volume
         $msource = $msource.DriveLetter + ":"
     }
     # run kape, adjust modules as necessary
     & $using:Kape --msource $msource --mdest $mdest --mflush --module !EZParser --mef csv 2>&1 | Out-Null
     # clean-up
-    if ($_.TriageType -eq 'DupTriage') {
+    if ($_.TriageType -eq "DupTriage") {
         Remove-Item $msource -Recurse -Force
     }
-    if ($_.TriageType -eq 'KapeTriage') {
+    if ($_.TriageType -eq "KapeTriage") {
         Dismount-VHD -Path $vhdx
         Remove-Item -Path $vhdx
     }
