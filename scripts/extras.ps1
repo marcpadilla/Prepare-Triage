1a07818f9b6739334314800628a717af9d5d3dd0
Marc Padilla <marc@padil.la>
2020-08-19 19:50:44 -0700

    testing

--- scripts/extras.ps1
+++ scripts/extras.ps1
@@ -8,9 +8,32 @@ $ExtrasDest = $mdest + "\Extras\"
 New-Item -Path $ExtrasDest -ItemType Directory 2>&1 | Out-Null
 
 # Services
-$Services = Get-ChildItem -Recurse -Path $DataDirectory -Filter "System.evtx" | ForEach-Object {
-    Get-WinEvent -FilterHashtable @{ Path = $_.FullName ; ProviderName = "Service Control Manager" ; Id = 7045 }
+$Svs = Get-ChildItem -Recurse -Path $DataDirectory -Filter "System.evtx" | ForEach-Object {
+    Get-WinEvent -FilterHashtable @{ Path = $_.FullName ; Id = 7045 } | ForEach-Object {
+        $Time = ([xml]$_.ToXml()).GetElementsByTagName("TimeCreated").itemOf(0)
+        $ServiceName = ([xml]$_.ToXml()).GetElementsByTagName("Data").itemOf(0) | Select -ExpandProperty "#text"
+        $ServicePath = ([xml]$_.ToXml()).GetElementsByTagName("Data").itemOf(1) | Select -ExpandProperty "#text"
+        $EventDetails =  $ServiceName + ", " + $ServicePath
+        [PsCustomObject][ordered]@{
+            Time = [string]$Time.SystemTime.Replace("T", " ").Split(".")[0] ;
+            Source = "System:7045" ;
+            Hostname = $using:mdest.Parent.Name ;
+            HostIpAddress = $HostIpAddress ;
+            UserId = "" ;
+            Assessment = "Context" ;
+            SourceRelevance = "Service Creation" ;
+            EventDetails = $EventDetails ;
+            SourceIpAddress = "" ;
+            Comments = "Service Created" ;
+            Hash = "" ;
+            EventAddedBy = "MP" ;
+            DateAdded = Get-Date -Format yyyy-MM-dd
+        }
+    }
 }
+
+<#
+
 $Services | Export-Csv -Path $ExtrasDest"services.csv" -Encoding ascii
 
 # SUCCESSFULL RDP Information
@@ -45,6 +68,8 @@ $records += Get-ChildItem -Recurse -Path $DataDirectory -Filter "Microsoft-Windo
     }
 }
 $records | Export-Csv -Path $ExtrasDest"rdp_successful.csv" -Encoding ascii
-$records | Select-Object -Property SourceIp | Sort-Object SourceIp -Unique | Export-Csv -Path $ExtrasDest"rdp_inbound_unique_ips.csv" -Encoding ascii
+#$records | Select-Object -Property SourceIp | Sort-Object SourceIp -Unique | Export-Csv -Path $ExtrasDest"rdp_inbound_unique_ips.csv" -Encoding ascii
 # dedupe! clean up source
 Write-Output "Extras complete.`n"
+
+#>
