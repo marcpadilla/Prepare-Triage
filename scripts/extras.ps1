05c9c3be0d89afeb2d87ac696567705ab5416254
Marc Padilla <marc@padil.la>
2020-08-19 21:07:34 -0700

    test

--- scripts/extras.ps1
+++ scripts/extras.ps1
@@ -60,7 +60,6 @@ $SuccessfulRemoteLogins = Get-ChildItem -Recurse -Path $DataDirectory -Filter "S
     }
 }
 $SuccessfulRemoteLogins += Get-ChildItem -Recurse -Path $DataDirectory -Filter "Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" | ForEach-Object {
-    $EventLog = $_.FullName
     Get-WinEvent -FilterHashtable @{ Path = $_.FullName ; Id = 1149 } | ForEach-Object {
         $Time = ([xml]$_.ToXml()).GetElementsByTagName("TimeCreated").itemOf(0)
         $SourceIpAddress = ([xml]$_.ToXml()).GetElementsByTagName("EventXML").itemOf(0) | Select -ExpandProperty "Param3"
@@ -83,10 +82,9 @@ $SuccessfulRemoteLogins += Get-ChildItem -Recurse -Path $DataDirectory -Filter "
     }
 }
 
-$SuccessfulRemoteLogins | Export-Csv -Path $ExtrasDest"successul_remote_logins.csv" -Encoding ascii
+if ($SuccessfulRemoteLogins) { $SuccessfulRemoteLogins | Export-Csv -Path $ExtrasDest"successul_remote_logins.csv" -Encoding ascii }
 
-
-Get-WinEvent -FilterHashtable @{ Path = "C:\Windows\System32\winevt\Logs\Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" ; Id = 1149 } | ForEach-Object { ([xml]$_.ToXml()).GetElementsByTagName("EventXML") | Select -ExpandProperty "Param3" }
+#Get-WinEvent -FilterHashtable @{ Path = "C:\Windows\System32\winevt\Logs\Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" ; Id = 1149 } | ForEach-Object { ([xml]$_.ToXml()).GetElementsByTagName("EventXML") | Select -ExpandProperty "Param3" }
 <#
 $DataDirectory = "C:\Windows\System32\winevt\Logs\"
 $HostName = "ExampleHost"
